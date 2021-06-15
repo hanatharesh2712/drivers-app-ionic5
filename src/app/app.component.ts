4cb3745626038fe9c4bb718902f55bacf3c71722
amartino24 <amartino@mroots.com.ar>
2021-06-15 16:58:39 -0300

    version 2.3.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,7 +61,7 @@ export class AppComponent {
   ];
   public loggedInUser: User;
   storageUrl: string = environment.storageUrl;
-  version = '2.3.7';
+  version = '2.3.8';
   constructor(
     public platform: Platform,
     public splashScreen: SplashScreen,
